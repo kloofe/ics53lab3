3d01f121f0ad8769fd62bf09f270335a9b911301
Nina V <ninavolkmuth@gmail.com>
2016-05-25 17:44:18 -0700

    Checking end server input

--- lab4/proxy.c
+++ lab4/proxy.c
@@ -1,5 +1,17 @@
 #include "csapp.h"
 
+void foo(int fd) {
+	struct stat sbuf;
+	char buf[1024], method[1024], uri[1024], version[1024];
+	char filename[1024], cgiargs[1024];
+	rio_t rio;
+
+	Rio_readinitb(&rio, fd);
+	Rio_readlineb(&rio, buf, 1024);
+	sscanf(buf, "%s %s %s", method, uri, version);
+	printf("%s %s %s\n", method, uri, version);
+}
+
 int main(int argc, char **argv) {
 	int listenfd, connfd;
 	char hostname[1024], port[1024];
@@ -16,6 +28,7 @@ int main(int argc, char **argv) {
 		connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen);
 		Getnameinfo((SA *) &clientaddr, clientlen, hostname, 1024, port, 1024, 0);
 		printf("Accepted connection from (%s, %s)\n", hostname, port);
+		foo(connfd);
 		Close(connfd);
 	}
 	return 0;
